b937044e9f29db04f26e8ea5527c04f563e39ba4
Gompou Tia Gbongué Ange <35264040+AngeTia@users.noreply.github.com>
2023-02-02 18:08:50 +0000

    Update index.php

--- Exercise_1/index.php
+++ Exercise_1/index.php
@@ -1,38 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <?php
-            $balls = 164; 
-            // $firstValue = 2400;
-            // $secondValue = 164;
-            // $firdValue = 945;
-            // $fordValue = 5;
-            function totalOvers($balls)
-            {
-                if ($balls % 6 == 0) {
-                    echo $balls/6;
-                } elseif ($balls == 164)
-                {
-                    echo $balls/6.029;
-                }
-                elseif ($balls == 945)
-                {
-                    echo $balls/6.0076;
-                } elseif ($balls == 5)
-                {
-                    echo $balls/10;
-                } else {
-                    echo "Nombre inconnu";
-                }
-                
-            }
-        totalOvers($balls);
-        ?>
-    </body>
-</html>
+<?php
+
+function totalOvers(int $balls): float | int | string
+{
+    $overs = floor($balls / 6);
+    $ball = $balls % 6;
+
+    return (float) $overs.'.'.$ball;
+}
+echo totalOvers(164);
